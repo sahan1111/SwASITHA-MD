53b9c294b905d0a42fe291c50d8f010811bd773a
Dark_Sahan-x_team <120746548+sahan1111@users.noreply.github.com>
2024-10-25 06:00:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,3 +8,4 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=v65hxZTD#1uQ0rQKA2XdkFlFNJpHMruhssJSfU9R9SPtP-Xa-mg8",
 MONGODB: process.env.MONGODB || "mongodb+srv://ponnapkaya1:CUfG1C4P0RUfFsSk@cluster0.7qsf0.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
+.
